1043223315393a4ffa5afaa2430ab4aaa6106242
keass <917313843@qq.com>
2018-11-06 10:23:08 +0800

    解决冲突啦

---
+++ FlowImage/FlowImage/FlowImageLayout.h
@@ -0,0 +1,13 @@
+//
+//  FlowImageLayout.h
+//  FlowImage
+//
+//  Created by 何松泽 on 2018/11/6.
+//  Copyright © 2018年 HSZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlowImageLayout : NSObject
+
+@end
---
+++ FlowImage/FlowImage/FlowImageLayout.m
@@ -0,0 +1,13 @@
+//
+//  FlowImageLayout.m
+//  FlowImage
+//
+//  Created by 何松泽 on 2018/11/6.
+//  Copyright © 2018年 HSZ. All rights reserved.
+//
+
+#import "FlowImageLayout.h"
+
+@implementation FlowImageLayout
+
+@end
